590601a287593fe1bb2e5536fb83174624ee3408
Fred <m.aminshahin@gmail.com>
2024-06-24 15:38:49 +0330

    first database data

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    #image
+    #author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    #tag
+    #category
+    counted_view = models.IntegerFeild(default=0)
+
