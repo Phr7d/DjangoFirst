ac3f687a17bc18dae68163dcd777c100a08ccfe2
Fred <147978369+Phr7d@users.noreply.github.com>
2024-06-28 19:23:58 +0330

    view counts

--- blog/admin.py
+++ blog/admin.py
@@ -4,5 +4,5 @@ from blog.models import Post
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
-    list_display = ('title','status','content')
+    list_display = ('title','status','counted_view','content')
 admin.site.register(Post,PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -2,25 +2,32 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from blog.models import Post
 from django.utils import timezone
+from django.db.models import Q
+
+
 # Create your views here.
 
 def blog_view(request):
     #Show posts from ordering time pulished date
-    posts = Post.objects.filter(pulished_date__lte=timezone.now())
+    posts = Post.objects.filter(Q(status=True) & Q(pulished_date__lte=timezone.now()))
     context = {'postss': posts}
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
-    posts = get_object_or_404(Post,pk=pid)
-    context = {'post': posts}
-    return render(request,'blog/blog-single.html',context)
+    try:
+        posts = Post.objects.filter(Q(status=True) & Q(id=pid) & Q(pulished_date__lte=timezone.now()))
+        if posts:
+            #get post from slug
+            posts = Post.objects.get(id = pid)
+            #add to count view
+            posts.counted_view = posts.counted_view + 1
+            #Save counted view
+            posts.save()
+        context = {'post': posts}
+        return render(request,'blog/blog-single.html',context)
+    except:
+        posts = get_object_or_404(Post,pk=0)
 
-def test_view(request,pid):
-    #get post from slug
-    posts = Post.objects.get(id = pid)
-    #add to count view
-    posts.counted_view = posts.counted_view + 1
-    #Save counted view
-    posts.save()
-    context = {'postss': posts}
-    return render(request,'website/test.html',context)
+# def test_view(request,pid):
+#     context = {'postss': posts}
+#     return render(request,'website/test.html',context)
